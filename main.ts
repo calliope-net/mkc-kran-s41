88162693ade978d0e943ddc9e62cdf93d60b67a0
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-18 19:19:08 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -15,6 +15,7 @@ function rgb_anzeigen () {
 if (!(radio.simulator())) {
     radio.beimStart(239, true)
     radio.enableButtonSendReset(true)
+    radio.enableButtonMotor1(true)
     lcd16x2rgb.initLCD(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E))
 }
 loops.everyInterval(400, function () {
