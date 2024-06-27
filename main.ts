12770eb961b93507e0657ecb2138571423a2e739
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-06-27 18:22:03 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,29 +28,27 @@ storage.putNumber(StorageSlots.s1, radio.getFunkgruppe(0))
 loops.everyInterval(400, function () {
     rgb_anzeigen()
     if (sender.joystickQwiic()) {
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 0, 3, sender.joystickValue(sender.eJoystickValue.xmotor, 5))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 4, 7, sender.joystickValue(sender.eJoystickValue.servo16, 5))
-        lcd16x2rgb.writeText(lcd16x2rgb.lcd16x2_eADDR(lcd16x2rgb.eADDR_LCD.LCD_16x2_x3E), 0, 8, 15, sender.joystickButtonOn())
+    	
     }
     if (sender.isSwitch(sender.eStatus.nicht_angeschlossen) && sender.joystickButtonOn()) {
         if (!(sender.getGabelstapler())) {
             radio.fill_sendBuffer19()
-            sender.sendM0(radio.radio_sendBuffer19(), 5, 10)
+            sender.sendM0(radio.radio_sendBuffer19())
             radio.setSchalter(radio.radio_sendBuffer19(), radio.e0Schalter.b0, !(input.buttonIsPressed(Button.A)) && input.buttonIsPressed(Button.B))
             radio.sendData(radio.radio_sendBuffer19())
         } else {
             radio.fill_sendBuffer19()
-            sender.sendM01(radio.radio_sendBuffer19(), 5, 60)
+            sender.sendM01(radio.radio_sendBuffer19())
             radio.sendData(radio.radio_sendBuffer19())
         }
     } else if (sender.i2cReadSwitch()) {
         radio.fill_sendBuffer19()
         if (sender.isSwitch(sender.eStatus.fahren)) {
-            sender.sendM0(radio.radio_sendBuffer19(), 5, 10)
+            sender.sendM0(radio.radio_sendBuffer19())
         } else if (sender.isSwitch(sender.eStatus.seilrolle)) {
-            sender.sendMAB(radio.radio_sendBuffer19(), 5)
+            sender.sendMAB(radio.radio_sendBuffer19())
         } else if (sender.isSwitch(sender.eStatus.zahnstange)) {
-            sender.sendMCB(radio.radio_sendBuffer19(), 5)
+            sender.sendMCB(radio.radio_sendBuffer19())
         }
         radio.setSchalter(radio.radio_sendBuffer19(), radio.e0Schalter.b0, !(input.buttonIsPressed(Button.A)) && input.buttonIsPressed(Button.B))
         radio.setSchalter(radio.radio_sendBuffer19(), radio.e0Schalter.b1, sender.getMagnet())
